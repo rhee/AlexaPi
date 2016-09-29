2bd1c529bdcec69672356b1c93720706130f27b5
Sang-Hoon RHEE <shr386+github@hotmail.com>
2016-09-29 19:06:44 +0900

    .

--- main.py
+++ main.py
@@ -31,27 +31,32 @@ sound_chime1 = os.path.join(path,'sounds/chime1.mp3') # listening
 sound_chime2 = os.path.join(path,'sounds/chime2.mp3') # querying
 sound_chime3 = os.path.join(path,'sounds/chime3.mp3') # something wrong
 
-ramdir = '/dev/shm/alexa-pi'
-try: os.makedirs(os.path.join(ramdir,'bak'))
+alexa_tmp = '/tmp/alexa-pi'
+
+if sys.platform.startswith('linux'):
+  alexa_tmp = '/dev/shm/alexa-pi'
+
+try: os.makedirs(os.path.join(alexa_tmp,'bak'))
 except: pass
 
-raw_recording = os.path.join(ramdir,'recording.raw')
-raw_recording_bak = os.path.join(ramdir,'bak/recording.raw')
-mp3_response = os.path.join(ramdir,'response.mp3')
-mp3_response_bak = os.path.join(ramdir,'bak/response.mp3')
-http_log = os.path.join(ramdir,'http.log')
-http_log_bak = os.path.join(ramdir,'bak/http.log')
+raw_recording = os.path.join(alexa_tmp,'recording.raw')
+raw_recording_bak = os.path.join(alexa_tmp,'bak/recording.raw')
+mp3_response = os.path.join(alexa_tmp,'response.mp3')
+mp3_response_bak = os.path.join(alexa_tmp,'bak/response.mp3')
+http_log = os.path.join(alexa_tmp,'http.log')
+http_log_bak = os.path.join(alexa_tmp,'bak/http.log')
 
 
 
 
-# handle alsa-lib error log things
-from ctypes import CFUNCTYPE,cdll,c_char_p, c_int
-ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
-def py_error_handler(filename, line, function, err, fmt): pass #print 'messages are yummy'
-c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
-asound = cdll.LoadLibrary('libasound.so')
-asound.snd_lib_error_set_handler(c_error_handler)
+if sys.platform.startswith('linux'):
+    # handle alsa-lib error log things
+    from ctypes import CFUNCTYPE,cdll,c_char_p, c_int
+    ERROR_HANDLER_FUNC = CFUNCTYPE(None, c_char_p, c_int, c_char_p, c_int, c_char_p)
+    def py_error_handler(filename, line, function, err, fmt): pass #print 'messages are yummy'
+    c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
+    asound = cdll.LoadLibrary('libasound.so')
+    asound.snd_lib_error_set_handler(c_error_handler)
 
 
 
@@ -82,7 +87,7 @@ def handle():
         except: pass
 
         if os.path.exists('etc/backup-log.sh'):
-            try: os.system('sh etc/backup-log.sh')
+            try: os.system('env ALEXA_TMP='+alexa_tmp+' sh etc/backup-log.sh')
             except: pass
 
         logging.info('finished alexa()')
