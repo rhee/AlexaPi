162efab8f83c92cbb69f30c56d39f7f168e9753d
Sang-Hoon RHEE <shr386+github@hotmail.com>
2016-10-26 14:53:11 +0000

    .

--- snowboy/__init__.py
+++ snowboy/__init__.py
@@ -3,7 +3,12 @@ import sys
 if sys.platform.startswith('linux'):
     if sys._multiarch == 'arm-linux-gnueabihf':
         print 'arm-linux-gnueabihf detected'
-        from rpi import snowboydecoder
+        # try import rpi3 version
+        try:
+            from rpi import snowboydecoder
+        except:
+            # try tk1 version, if rpi3 version fails
+            from tk1 import snowboydecoder
     else:
         from linux import snowboydecoder
 
